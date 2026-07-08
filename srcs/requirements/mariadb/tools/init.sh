ccb9eecd852d5b890985284d0a834add3584e236
atashiro <atashiro@atashiro.42.fr>
2026-07-08 19:23:26 +0900

    fin wordpress

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -2,9 +2,12 @@
 set -e
 
 mkdir -p /run/mysqld
-chown mysql:mysql /run/mysqld
+chown -R mysql:mysql /run/mysqld
+
+echo ">>> init.sh 開始!"
 
 if [ ! -d /var/lib/mysql/mysql ]; then
+	echo ">>> mariaDBを初期化..."
 	mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 	mariadbd --user=mysql --skip-networking &
 	until mariadb-admin ping --silent; do sleep 1; done
@@ -14,5 +17,13 @@ CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+DELETE FROM mysql.user WHERE User='';
 FLUSH PRIVILEGES;
 EOF
+
+	mariadb-admin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+fi
+
+echo ">>> MariaDB起動"
+
+exec mariadbd --user=mysql --skip-networking=0
---
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+until mariadb -h mariadb -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SELECT 1" >/dev/null 2>&1; do
+
+	echo "MariaDBの起動を待っています..."
+
+	sleep 2
+
+done
+
+
+if [ ! -f wp-config.php ]; then
+
+	wp core download --allow-root
+
+	wp config create --allow-root \
+		--dbname="$MYSQL_DATABASE" \
+		--dbuser="$MYSQL_USER" \
+		--dbpass="$MYSQL_PASSWORD" \
+		--dbhost=mariadb
+
+	wp core install --allow-root \
+		--url="$WP_URL" \
+		--title="$WP_TITLE" \
+		--admin_user="$WP_ADMIN_USER" \
+		--admin_password="$WP_ADMIN_PASSWORD" \
+		--admin_email="$WP_ADMIN_EMAIL"
+
+	wp user create --allow-root "$WP_USER" "$WP_USER_EMAIL" \
+		--user_pass="$WP_USER_PASSWORD" --role=author
+
+fi
+
+
+exec php-fpm83 -F
